810e2a98ffaa646f69100e4f310d106c9c33def5
Nick Pace <nicholasppace@gmail.com>
2024-01-27 10:36:42 -0500

    Added a bunch of content, but no styles

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "./logo.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <h1>NavBar</h1>
+      <Image
+        src={Logo}
+        alt="logo"
+        width={100}
+        height={100}
+        quality={100}
+        placeholder="blur"
+      />
+      <h1>Nicks Resume</h1>
       <Link href="/">Home</Link>
+      <Link href="/resume">Resume</Link>
       <Link href="/sites">Sites</Link>
       <Link href="/contact">Contact</Link>
     </nav>
---
+++ app/contact/page.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <div>
+      <div>Hit em with the intro</div>
+      <div>Name field</div>
+      <div>email field</div>
+      <div>message field</div>
+    </div>
+  );
+}
