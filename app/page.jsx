3f78d9d0341f678e1728c4ae54b6d2d33b2ffe2c
NICK PACE <nicholasppace@gmail.com>
2024-02-06 10:44:49 -0500

    Set up tailwind and began styling

--- app/page.jsx
+++ app/page.jsx
@@ -1,33 +1,32 @@
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col items-center">
       <div>Welcome!</div>
-      <div>
+      <div className="text-center">
         Hey there! My name is Nick and I am an aspiring web developer. I've done
         IT for close to 10 years now and am looking to branch out to new
         horizons.
-        <div>
-          The bulk of my experience with IT has been focused on supporting
-          various different military commands and government agencies. I've
-          always been interested in creating things and problem solving.
-          Inherently, the nature of IT is about problem solving, so it was an
-          easy transition to that as a career. However, it never truly felt like
-          home because I was always fixing something that was broken rather than
-          creating new stuff. After some convincing, one of my closest friends
-          got me to sit down and take some courses on JavaScript and I fell in
-          love with both the creation and problem solving. I've been completely
-          "self-taught". Which is to say a lot of researching issues, a lot of
-          self paced courses, and a lot of collaboration with my friends who are
-          in the industry to develop new skills and work my way towards doing
-          this as a full-time job.
-        </div>
-        <div></div>
-        <div>
-          If you'd like to see my work, head on over to the resume section; if
-          you'd like to get to know me better, head to the About section; and if
-          you're looking to reach out, head over to the, you guessed it, contact
-          section.
-        </div>
+      </div>
+      <div>
+        The bulk of my experience with IT has been focused on supporting various
+        different military commands and government agencies. I've always been
+        interested in creating things and problem solving. Inherently, the
+        nature of IT is about problem solving, so it was an easy transition to
+        that as a career. However, it never truly felt like home because I was
+        always fixing something that was broken rather than creating new stuff.
+        After some convincing, one of my closest friends got me to sit down and
+        take some courses on JavaScript and I fell in love with both the
+        creation and problem solving. I've been completely "self-taught". Which
+        is to say a lot of researching issues, a lot of self paced courses, and
+        a lot of collaboration with my friends who are in the industry to
+        develop new skills and work my way towards doing this as a full-time
+        job.
+      </div>
+      <div>
+        If you'd like to see my work, head on over to the resume section; if
+        you'd like to get to know me better, head to the About section; and if
+        you're looking to reach out, head over to the, you guessed it, contact
+        section.
       </div>
     </main>
   );
