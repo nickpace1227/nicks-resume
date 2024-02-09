edcd9b1c7bdd9695d8b8d72fd55c5e593d63ed37
Nick Pace <nicholasppace@gmail.com>
2024-02-09 15:27:41 -0500

    header changes

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -4,27 +4,29 @@ import Logo from "./logo.png";
 
 export default function Header() {
   return (
-    <div className="flex h-24 top-0 absolute w-full bg-stone-500">
-      <Image
-        src={Logo}
-        alt="logo"
-        quality={100}
-        placeholder="blur"
-        className="m-2 h-16 w-16"
-      />
-      <div>
-        <h1 className="m-2">Nick's Resume</h1>
-        <nav>
-          <Link href="/" className="m-2">
-            Home
-          </Link>
-          <Link href="/mySites" className="m-2">
-            Sites
-          </Link>
-          <Link href="/contact" className="m-2">
-            Contact
-          </Link>
-        </nav>
+    <div className="h-24 top-0 absolute w-full bg-stone-500">
+      <div className="flex justify-center items-center">
+        <Image
+          src={Logo}
+          alt="logo"
+          quality={100}
+          placeholder="blur"
+          className="m-2 h-16 w-16"
+        />
+        <div>
+          <h1 className="m-2">Nick's Resume</h1>
+          <nav>
+            <Link href="/" className="m-2">
+              Home
+            </Link>
+            <Link href="/mySites" className="m-2">
+              Sites
+            </Link>
+            <Link href="/contact" className="m-2">
+              Contact
+            </Link>
+          </nav>
+        </div>
       </div>
     </div>
   );
